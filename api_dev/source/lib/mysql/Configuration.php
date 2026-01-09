5c8dc28c8c9db0057a8ad18bd4116cdb51c2ca14
darthjee <darthjee@gmail.com>
2026-01-09 14:38:04 -0300

    Add connect

--- api_dev/source/lib/mysql/Configuration.php
+++ api_dev/source/lib/mysql/Configuration.php
@@ -22,6 +22,21 @@ class Configuration
         return self::$instance;
     }
 
+    public static function connect()
+    {
+        $config = self::getInstance();
+        
+        $dsn = "mysql:host={$config->host};port={$config->port};dbname={$config->database};charset=utf8mb4";
+        
+        $pdo = new \PDO($dsn, $config->username, $config->password, [
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+            \PDO::ATTR_EMULATE_PREPARES => false,
+        ]);
+        
+        return $pdo;
+    }
+
     public function __construct($host, $database, $username, $password, $port = 3306)
     {
         $this->host = $host;
