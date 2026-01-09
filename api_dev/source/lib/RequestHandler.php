223f5b83f0ab5b8e97dc740d61c6914dd02dcbdd
darthjee <darthjee@gmail.com>
2026-01-09 13:26:31 -0300

    Add requesthandler

---
+++ api_dev/source/lib/RequestHandler.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ApiDev;
+
+class RequestHandler
+{
+    public function handle($request)
+    {
+        $configurations = Configuration::getConfigurations();
+
+        foreach ($configurations as $config) {
+            if ($config->match($request)) {
+                return $config->handle($request);
+            }
+        }
+
+        return null;
+    }
+
+    private function sendResponse($response)
+    {
+        http_response_code($response->httpCode);
+        foreach ($response->headerLines as $header) {
+            header($header);
+        }
+        echo $response->body;
+    }
+}
