2dca3d59882de4a96f7e741b335048a8284e26ae
darthjee <darthjee@gmail.com>
2026-01-09 13:20:31 -0300

    Refactor configuration

--- api_dev/source/lib/Configuration.php
+++ api_dev/source/lib/Configuration.php
@@ -4,14 +4,38 @@ namespace ApiDev;
 
 class Configuration
 {
+    private static $instance;
     private $configurations = [];
 
-    public function add($requestMethod, $path, $endpoint)
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new Configuration();
+        }
+        return self::$instance;
+    }
+
+    public static function add($requestMethod, $path, $endpoint)
+    {
+        return self::getInstance()->addRoute($requestMethod, $path, $endpoint);
+    }
+
+    public static function getConfigurations()
+    {
+        return self::getInstance()->fetchConfigurations();
+    }
+
+    public static function reset()
+    {
+        self::$instance = null;
+    }
+
+    public function addRoute($requestMethod, $path, $endpoint)
     {
         $this->configurations[] = new RouteConfiguration($requestMethod, $path, $endpoint);
     }
 
-    public function getConfigurations()
+    public function fetchConfigurations()
     {
         return $this->configurations;
     }
