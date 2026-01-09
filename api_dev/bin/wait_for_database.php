a8f8a1318c77d8a00ed1bf949aace27fcbbcf03c
darthjee <darthjee@gmail.com>
2026-01-09 18:58:57 -0300

    Put inside a loop

--- api_dev/bin/wait_for_database.php
+++ api_dev/bin/wait_for_database.php
@@ -7,11 +7,11 @@ $host = getenv('API_DEV_MYSQL_HOST') ?: 'localhost';
 $user = getenv('API_DEV_MYSQL_USER') ?: 'root';
 $password = getenv('API_DEV_MYSQL_PASSWORD') ?: '';
 $port = getenv('API_DEV_MYSQL_PORT') ?: 3306;
-
 $database = getenv('API_DEV_MYSQL_TEST_DATABASE') ?: 'test_db';
 
-\ApiDev\Mysql\Configuration::databaseExists(
-    $host, $user, $password, $port, $database
-);
+while (!\ApiDev\Mysql\Configuration::databaseExists($host, $user, $password, $port, $database)) {
+    echo "Waiting for database '$database'...\n";
+    sleep(1);
+}
 
 echo "Database '$database' ensured!\n";
