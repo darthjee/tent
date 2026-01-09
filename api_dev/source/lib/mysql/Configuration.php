6ce21b04bae4ad58892ef0840f435282a0d8f118
darthjee <darthjee@gmail.com>
2026-01-09 15:37:03 -0300

    Lint fixes

--- api_dev/source/lib/mysql/Configuration.php
+++ api_dev/source/lib/mysql/Configuration.php
@@ -25,15 +25,15 @@ class Configuration
     public static function connect()
     {
         $config = self::getInstance();
-        
+
         $dsn = "mysql:host={$config->host};port={$config->port};dbname={$config->database};charset=utf8mb4";
-        
+
         $pdo = new \PDO($dsn, $config->username, $config->password, [
             \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
             \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
             \PDO::ATTR_EMULATE_PREPARES => false,
         ]);
-        
+
         return new Connection($pdo);
     }
 
