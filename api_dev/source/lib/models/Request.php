f48d3886957ed49636ddd00ff0e62d06568d1af6
darthjee <darthjee@gmail.com>
2026-01-09 11:54:56 -0300

    Add models

---
+++ api_dev/source/lib/models/Request.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ApiDev;
+
+class Request
+{
+    public function requestMethod()
+    {
+        return $_SERVER['REQUEST_METHOD'];
+    }
+
+    public function body()
+    {
+        return file_get_contents('php://input');
+    }
+
+    public function headers()
+    {
+        return getallheaders();
+    }
+
+    public function requestUrl()
+    {
+        $uri = $_SERVER['REQUEST_URI'];
+        $parts = parse_url($uri);
+        return $parts['path'] ?? '/';
+    }
+
+    public function query()
+    {
+        $uri = $_SERVER['REQUEST_URI'];
+        $parts = parse_url($uri);
+        return $parts['query'] ?? '';
+    }
+}
