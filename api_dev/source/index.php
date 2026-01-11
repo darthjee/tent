ea0418b7998165d6399ef0a0ce60796d500eba78
darthjee <darthjee@gmail.com>
2026-01-11 19:38:50 -0300

    Use env database

--- api_dev/source/index.php
+++ api_dev/source/index.php
@@ -25,7 +25,7 @@ Configuration::add('GET', '/persons', ListPersonsEndpoint::class);
 
 \ApiDev\Mysql\Configuration::configure(
     getenv('API_DEV_MYSQL_HOST') ?: 'localhost',
-    getenv('API_DEV_MYSQL_TEST_DATABASE') ?: 'api_tent_test_db',
+    getenv('API_DEV_MYSQL_DEV_DATABASE') ?: 'api_tent_dev_db',
     getenv('API_DEV_MYSQL_USER') ?: 'root',
     getenv('API_DEV_MYSQL_PASSWORD') ?: '',
     getenv('API_DEV_MYSQL_PORT') ?: 3306
