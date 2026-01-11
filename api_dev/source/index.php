ebf43df89c45b601d974f712b84f001f276ac4df
darthjee <darthjee@gmail.com>
2026-01-11 18:58:16 -0300

    Add persons endpoint

--- api_dev/source/index.php
+++ api_dev/source/index.php
@@ -10,12 +10,15 @@ require_once __DIR__ . '/lib/api_dev/models/Response.php';
 require_once __DIR__ . '/lib/api_dev/models/MissingResponse.php';
 require_once __DIR__ . '/lib/api_dev/Route.php';
 require_once __DIR__ . '/lib/api_dev/Endpoint.php';
-require_once __DIR__ . '/lib/api_dev/endpoints/HealthCheckEndpoint.php';
 require_once __DIR__ . '/lib/api_dev/RouteConfiguration.php';
 require_once __DIR__ . '/lib/api_dev/Configuration.php';
 require_once __DIR__ . '/lib/api_dev/RequestHandler.php';
 
+require_once __DIR__ . '/lib/api_dev/endpoints/HealthCheckEndpoint.php';
+require_once __DIR__ . '/lib/api_dev/endpoints/ListPersonsEndpoint.php';
+
 Configuration::add('GET', '/health', HealthCheckEndpoint::class);
+Configuration::add('GET', '/persons', ListPersonsEndpoint::class);
 
 $request = new Request();
 $handler = new RequestHandler();
