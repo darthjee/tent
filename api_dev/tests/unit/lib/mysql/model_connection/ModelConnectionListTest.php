b21ba64c6161e2944e5fc1c145a50c50c959497e
darthjee <darthjee@gmail.com>
2026-01-11 17:42:05 -0300

    Move tests to correct folder

--- api_dev/tests/unit/lib/mysql/model_connection/ModelConnectionListTest.php
+++ api_dev/tests/unit/lib/mysql/model_connection/ModelConnectionListTest.php
@@ -2,7 +2,7 @@
 
 namespace ApiDev\Tests;
 
-require_once __DIR__ . '/../../../support/tests_loader.php';
+require_once __DIR__ . '/../../../../support/tests_loader.php';
 
 use PHPUnit\Framework\TestCase;
 use ApiDev\Mysql\Connection;
