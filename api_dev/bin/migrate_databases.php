2bac0723dd082221a1e49b9fd1048c73e975f93e
darthjee <darthjee@gmail.com>
2026-01-11 15:30:24 -0300

    Include migration

--- api_dev/bin/migrate_databases.php
+++ api_dev/bin/migrate_databases.php
@@ -4,6 +4,7 @@
 require_once __DIR__ . '/../source/lib/mysql/Connection.php';
 require_once __DIR__ . '/../source/lib/mysql/Configuration.php';
 require_once __DIR__ . '/../source/lib/mysql/MigrationsProcessor.php';
+require_once __DIR__ . '/../source/lib/mysql/Migration.php';
 
 class DatabaseMigrater {
     private $database;
