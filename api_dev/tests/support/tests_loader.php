c2ab332484ee184607b46204d9033bb9e0d1843c
darthjee <darthjee@gmail.com>
2026-01-11 18:43:27 -0300

    Add persons test

--- api_dev/tests/support/tests_loader.php
+++ api_dev/tests/support/tests_loader.php
@@ -7,4 +7,6 @@ require_once __DIR__ . '/../../source/lib/mysql/MigrationsProcessor.php';
 require_once __DIR__ . '/../../source/lib/mysql/Configuration.php';
 require_once __DIR__ . '/../../source/lib/mysql/Connection.php';
 
+require_once __DIR__ . '/../../source/lib/api_dev/models/Person.php';
+
 require_once __DIR__ . '/database_initializer.php';
---
+++ api_dev/tests/unit/lib/api_dev/models/PersonAllTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ApiDev\Tests;
+
+require_once __DIR__ . '/../../../../support/tests_loader.php';
+
+use PHPUnit\Framework\TestCase;
+use ApiDev\Models\Person;
+use ApiDev\Mysql\Configuration;
+
+class PersonAllTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        $connection = Configuration::connect();
+        $connection->execute('DELETE FROM persons');
+        $connection->execute('INSERT INTO persons (first_name, last_name, birthdate) VALUES (?, ?, ?)', ['Alice', 'Smith', '1991-01-01']);
+        $connection->execute('INSERT INTO persons (first_name, last_name, birthdate) VALUES (?, ?, ?)', ['Bob', 'Jones', '1992-02-02']);
+        $connection->execute('INSERT INTO persons (first_name, last_name, birthdate) VALUES (?, ?, ?)', ['Carol', 'Brown', '1993-03-03']);
+    }
+
+    public function testAllReturnsAllPersons()
+    {
+        $persons = Person::all();
+        $this->assertIsArray($persons);
+        $this->assertCount(3, $persons);
+        $this->assertInstanceOf(Person::class, $persons[0]);
+        $names = array_map(function($p) { return $p->getFirstName(); }, $persons);
+        $this->assertContains('Alice', $names);
+        $this->assertContains('Bob', $names);
+        $this->assertContains('Carol', $names);
+    }
+}
