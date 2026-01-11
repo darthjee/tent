79e23bb8f979480bdb11f90ad1c1ed265d7b65f1
darthjee <darthjee@gmail.com>
2026-01-11 15:48:02 -0300

    Add list method

--- api_dev/source/lib/mysql/ModelConnection.php
+++ api_dev/source/lib/mysql/ModelConnection.php
@@ -13,6 +13,31 @@ class ModelConnection
         $this->tableName = $tableName;
     }
 
+    /**
+     * Lists rows from the table with optional limit and offset.
+     *
+     * @param array $options ['limit' => int, 'offset' => int]
+     * @return array
+     */
+    public function list(array $options = []): array
+    {
+        $sql = "SELECT * FROM {$this->tableName}";
+        $params = [];
+        if (isset($options['limit'])) {
+            $sql .= " LIMIT ?";
+            $params[] = (int)$options['limit'];
+        }
+        if (isset($options['offset'])) {
+            // If limit is not set, MySQL requires LIMIT for OFFSET
+            if (!isset($options['limit'])) {
+                $sql .= " LIMIT 18446744073709551615"; // MySQL max value for LIMIT
+            }
+            $sql .= " OFFSET ?";
+            $params[] = (int)$options['offset'];
+        }
+        return $this->connection->fetch($sql, $params);
+    }
+
     public function getConnection(): Connection
     {
         return $this->connection;
