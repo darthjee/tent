f7ca3a3c9094fb0c868c605c775436968e50ea49
darthjee <darthjee@gmail.com>
2026-01-11 18:19:54 -0300

    Map into Person

--- api_dev/source/lib/api_dev/models/Person.php
+++ api_dev/source/lib/api_dev/models/Person.php
@@ -7,6 +7,27 @@ use ApiDev\Mysql\Configuration;
 
 class Person
 {
+
+    /**
+     * Returns all rows from the 'persons' table.
+     *
+     * @return array
+     */
+    private $attributes = [];
+
+    public function __construct(array $attributes = [])
+    {
+        $this->attributes = $attributes;
+    }
+
+    public static function all(): array
+    {
+        $rows = self::getConnection()->list();
+        return array_map(function ($attrs) {
+            return new self($attrs);
+        }, $rows);
+    }
+    
     /**
      * Returns a ModelConnection for the 'persons' table.
      *
@@ -24,15 +45,5 @@ class Person
             'persons'
         );
         return self::$connection;
-    }
-
-    /**
-     * Returns all rows from the 'persons' table.
-     *
-     * @return array
-     */
-    public static function all(): array
-    {
-        return self::getConnection()->list();
     }
 }
