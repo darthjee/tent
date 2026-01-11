a45a8dacbd10c25755c1fb7d0803cc464c1fa37f
darthjee <darthjee@gmail.com>
2026-01-11 15:51:26 -0300

    Extract build

--- api_dev/source/lib/mysql/Connection.php
+++ api_dev/source/lib/mysql/Connection.php
@@ -11,9 +11,24 @@ class Connection
         $this->pdo = $pdo;
     }
 
-    public function getPdo()
+    /**
+     * Builds a new Connection from parameters.
+     */
+    public static function build($host, $port, $database, $username, $password)
     {
-        return $this->pdo;
+        $dsn_parts = [
+            "mysql:host={$host}",
+            "port={$port}",
+            "dbname={$database}",
+            "charset=utf8mb4"
+        ];
+        $dsn = implode(';', $dsn_parts);
+        $pdo = new \PDO($dsn, $username, $password, [
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+            \PDO::ATTR_EMULATE_PREPARES => false,
+        ]);
+        return new self($pdo);
     }
 
     public function query($sql, $params = [])
@@ -44,5 +59,10 @@ class Connection
     public function lastInsertId()
     {
         return $this->pdo->lastInsertId();
+    }
+
+    public function getPdo()
+    {
+        return $this->pdo;
     }
 }
