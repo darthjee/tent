adae10fad66d742068f5deee4b27bd79f44fbf59
darthjee <darthjee@gmail.com>
2026-01-14 16:55:50 -0300

    Add documentation to StaticFileHandler

--- source/source/lib/handlers/StaticFileHandler.php
+++ source/source/lib/handlers/StaticFileHandler.php
@@ -2,15 +2,31 @@
 
 namespace Tent;
 
+/**
+ * FileHandler that serves static files based on the request URL and a base directory.
+ *
+ * This handler returns the contents of a file located by combining the base directory
+ * (provided by FolderLocation) and the requestUrl from the incoming request. It is
+ * typically used to serve static assets such as HTML, CSS, JS, images, etc.
+ */
 class StaticFileHandler extends FileHandler
 {
     private $folderLocation;
 
+    /**
+     * @param FolderLocation $folderLocation The base directory for static files.
+     */
     public function __construct($folderLocation)
     {
         $this->folderLocation = $folderLocation;
     }
 
+    /**
+     * Returns the file path for the static file to be served, based on the request URL.
+     *
+     * @param Request $request The incoming HTTP request.
+     * @return string The full file path to the static asset.
+     */
     protected function getFilePath($request)
     {
         return $this->folderLocation->basePath() . $request->requestUrl();
