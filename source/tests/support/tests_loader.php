c1a22e32e845d243a21317d0fa173b831898bf73
darthjee <darthjee@gmail.com>
2026-01-14 09:20:36 -0300

    Use tests_loaded on CurlHttpClientTest.php

--- source/tests/support/tests_loader.php
+++ source/tests/support/tests_loader.php
@@ -7,3 +7,5 @@ require_once __DIR__ . '/../../source/lib/service/RequestProcessor.php';
 require_once __DIR__ . '/../../source/lib/handlers/MissingRequestHandler.php';
 require_once __DIR__ . '/../../source/lib/models/RequestMatcher.php';
 require_once __DIR__ . '/../../source/lib/models/Rule.php';
+require_once __DIR__ . '/../../source/lib/utils/CurlUtils.php';
+require_once __DIR__ . '/../../source/lib/http/CurlHttpClient.php';
