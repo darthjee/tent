53140b61b66749cd50b7116b13cbda93ef87d5ac
darthjee <darthjee@gmail.com>
2026-01-14 16:54:28 -0300

    Add documentation to MissingRequestHandler

--- source/source/lib/handlers/MissingRequestHandler.php
+++ source/source/lib/handlers/MissingRequestHandler.php
@@ -4,8 +4,20 @@ namespace Tent;
 
 use Tent\RequestHandler;
 
+/**
+ * RequestHandler that always returns a 404 response.
+ *
+ * This handler is used when no other handler matches the request. It always returns
+ * a MissingResponse, representing a 404 Not Found.
+ */
 class MissingRequestHandler implements RequestHandler
 {
+    /**
+     * Returns a MissingResponse (404 Not Found) for any request.
+     *
+     * @param Request $request The incoming HTTP request.
+     * @return MissingResponse The 404 response.
+     */
     public function handleRequest($request)
     {
         return new MissingResponse();
