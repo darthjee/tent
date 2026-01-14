df8e27ce1c1df2b7b14d6eef984a9ead408852a0
darthjee <darthjee@gmail.com>
2026-01-14 17:29:37 -0300

    Add documentation to CurlUtils

--- source/source/lib/utils/CurlUtils.php
+++ source/source/lib/utils/CurlUtils.php
@@ -2,8 +2,21 @@
 
 namespace Tent;
 
+/**
+ * Utility class for handling HTTP headers in cURL requests and responses.
+ *
+ * Used by CurlHttpClient to build request headers and parse response headers.
+ */
 class CurlUtils
 {
+    /**
+     * Builds an array of header lines for cURL from an associative array.
+     *
+     * Example: ['User-Agent' => 'Test'] becomes ['User-Agent: Test']
+     *
+     * @param array $headers Associative array of headers.
+     * @return string[] Array of header lines in "Key: Value" format.
+     */
     public static function buildHeaderLines($headers)
     {
         $headerLines = [];
@@ -13,6 +26,14 @@ class CurlUtils
         return $headerLines;
     }
 
+    /**
+     * Parses raw response headers from cURL into an array of header lines.
+     *
+     * Removes empty lines and HTTP status lines.
+     *
+     * @param string $headers Raw headers string from cURL response.
+     * @return string[] Array of header lines in "Key: Value" format.
+     */
     public static function parseResponseHeaders($headers)
     {
         $headerLines = explode("\n", $headers);
