810413a06ecde4db0353fb67c5a38da76cf754d7
darthjee <darthjee@gmail.com>
2026-01-14 17:01:55 -0300

    Add documentation to HttpClientInterface

--- source/source/lib/http/CurlHttpClient.php
+++ source/source/lib/http/CurlHttpClient.php
@@ -4,8 +4,22 @@ namespace Tent;
 
 use Tent\HttpClientInterface;
 
+/**
+ * HTTP client for proxying requests using cURL.
+ *
+ * This class is responsible for making HTTP requests to the target server when acting as a proxy.
+ * Currently, only GET requests are implemented. It uses cURL to perform the request and returns
+ * the response body, HTTP status code, and headers as an array.
+ */
 class CurlHttpClient implements HttpClientInterface
 {
+    /**
+     * Sends an HTTP GET request to the given URL with the provided headers.
+     *
+     * @param string $url The target URL for the request.
+     * @param array $headers The headers to send with the request.
+     * @return array An array with 'body', 'httpCode', and 'headers' from the response.
+     */
     public function request($url, $headers)
     {
         $headerLines = CurlUtils::buildHeaderLines($headers);
