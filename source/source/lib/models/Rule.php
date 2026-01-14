829ee97b48678ddbd21d8fe0efdbe18d72c29b9f
darthjee <darthjee@gmail.com>
2026-01-14 17:22:24 -0300

    Add documentation to Rule

--- source/source/lib/models/Rule.php
+++ source/source/lib/models/Rule.php
@@ -2,22 +2,52 @@
 
 namespace Tent;
 
+/**
+ * Represents a routing rule for processing HTTP requests.
+ *
+ * A Rule contains multiple RequestMatchers to validate if a request applies to this rule.
+ * When a request matches, the Rule provides the RequestHandler to process the request.
+ */
 class Rule
 {
+    /**
+     * @var RequestHandler The handler used to process matching requests.
+     */
     private $handler;
+
+    /**
+     * @var RequestMatcher[] List of matchers to validate if a request applies to this rule.
+     */
     private $matchers;
 
+    /**
+     * Constructs a Rule.
+     *
+     * @param RequestHandler $handler The handler to process requests that match this rule.
+     * @param RequestMatcher[] $matchers Array of matchers to validate requests.
+     */
     public function __construct($handler, $matchers = [])
     {
         $this->handler = $handler;
         $this->matchers = $matchers;
     }
 
+    /**
+     * Returns the RequestHandler for this rule.
+     *
+     * @return RequestHandler
+     */
     public function handler()
     {
         return $this->handler;
     }
 
+    /**
+     * Checks if the given request matches any of the rule's matchers.
+     *
+     * @param Request $request The incoming HTTP request.
+     * @return bool True if any matcher applies to the request.
+     */
     public function match($request)
     {
         foreach ($this->matchers as $matcher) {
