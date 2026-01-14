d7359426391fc4b4d2a4adf9f6601e4b84c2f694
darthjee <darthjee@gmail.com>
2026-01-14 15:31:01 -0300

    Lint fix

--- dev/api/bin/create_databases.php
+++ dev/api/bin/create_databases.php
@@ -5,7 +5,8 @@ require_once __DIR__ . '/../source/lib/mysql/Connection.php';
 require_once __DIR__ . '/../source/lib/mysql/Configuration.php';
 require_once __DIR__ . '/../source/lib/mysql/DatabaseInitializer.php';
 
-class DatabaseEnsurer {
+class DatabaseEnsurer
+{
     private $database;
 
     public function __construct($database) {
