b4efca2eef426c37aa56bd9666959633d345f2af
darthjee <darthjee@gmail.com>
2026-01-14 17:24:44 -0300

    Add documentation to Server

--- source/source/lib/models/Server.php
+++ source/source/lib/models/Server.php
@@ -2,15 +2,34 @@
 
 namespace Tent;
 
+/**
+ * Model representing the base address for proxy requests.
+ *
+ * Used by ProxyHandler to define the target host for forwarding requests.
+ * Similar to FolderLocation, this is a simple value object.
+ */
 class Server
 {
+    /**
+     * @var string The base address (host) for proxy requests.
+     */
     private $targetHost;
 
+    /**
+     * Constructs a Server model.
+     *
+     * @param string $targetHost The base address for proxy requests.
+     */
     public function __construct($targetHost)
     {
         $this->targetHost = $targetHost;
     }
 
+    /**
+     * Returns the base address (host) for proxy requests.
+     *
+     * @return string
+     */
     public function targetHost()
     {
         return $this->targetHost;
