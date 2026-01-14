61a89d903cb875ebba21fb2d2ae3d0261d86adac
darthjee <darthjee@gmail.com>
2026-01-14 09:22:40 -0300

    Use tests_loaded on StaticFileHandlerTest.php

--- source/tests/support/tests_loader.php
+++ source/tests/support/tests_loader.php
@@ -9,3 +9,6 @@ require_once __DIR__ . '/../../source/lib/models/RequestMatcher.php';
 require_once __DIR__ . '/../../source/lib/models/Rule.php';
 require_once __DIR__ . '/../../source/lib/utils/CurlUtils.php';
 require_once __DIR__ . '/../../source/lib/http/CurlHttpClient.php';
+require_once __DIR__ . '/../../source/lib/models/MissingResponse.php';
+require_once __DIR__ . '/../../source/lib/models/FolderLocation.php';
+require_once __DIR__ . '/../../source/lib/handlers/StaticFileHandler.php';
