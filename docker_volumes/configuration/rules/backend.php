d743b47b883b593eeb155ddd39838dbf866c3c5a
Fernando Favini <darthjee@gmail.com>
2026-01-16 17:16:52 -0300

    Refactor rules (#45)

---
+++ docker_volumes/configuration/rules/backend.php
@@ -0,0 +1,17 @@
+<?php
+
+use Tent\Configuration;
+use Tent\Models\Rule;
+use Tent\Handlers\ProxyRequestHandler;
+use Tent\Models\Server;
+use Tent\Models\RequestMatcher;
+
+Configuration::buildRule([
+    'handler' => [
+        'type' => 'proxy',
+        'host' => 'http://api:80'
+    ],
+    'matchers' => [
+        ['method' => 'GET', 'uri' => '/persons', 'type' => 'exact']
+    ]
+]);
