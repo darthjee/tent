3fccd5ae183931da561e496c107aaf6bfacb408d
darthjee <darthjee@gmail.com>
2026-01-16 13:11:45 -0300

    Split configuration file

---
+++ docker_volumes/configuration/rules/backend.php
@@ -0,0 +1,11 @@
+<?php
+
+use Tent\Configuration;
+use Tent\Models\Rule;
+use Tent\Handlers\ProxyRequestHandler;
+use Tent\Models\Server;
+use Tent\Models\RequestMatcher;
+
+require_once __DIR__ . '/rules/frontend.php';
+require_once __DIR__ . '/rules/backend.php';
+
---
+++ docker_volumes/configuration/rules/frontend.php
@@ -0,0 +1,44 @@
+<?php
+
+use Tent\Configuration;
+use Tent\Models\Rule;
+use Tent\Handlers\FixedFileHandler;
+use Tent\Handlers\ProxyRequestHandler;
+use Tent\Handlers\StaticFileHandler;
+use Tent\Models\Server;
+use Tent\Models\FolderLocation;
+use Tent\Models\RequestMatcher;
+
+if (getenv('FRONTEND_DEV_MODE') === 'true') {
+    Configuration::addRule(
+        new Rule(
+            new ProxyRequestHandler(new Server('http://frontend:8080')),
+            [
+                new RequestMatcher('GET', '/', 'exact'),
+                new RequestMatcher('GET', '/assets/js/', 'begins_with'),
+                new RequestMatcher('GET', '/assets/css/', 'begins_with'),
+                new RequestMatcher('GET', '/@vite/', 'begins_with'),
+                new RequestMatcher('GET', '/node_modules/', 'begins_with'),
+                new RequestMatcher('GET', '/@react-refresh', 'exact')
+            ]
+        )
+    );
+} else {
+    Configuration::addRule(
+        new Rule(
+            new StaticFileHandler(new FolderLocation('/var/www/html/static')),
+            [
+                new RequestMatcher('GET', '/index.html', 'exact'),
+                new RequestMatcher('GET', '/assets', 'begins_with'),
+            ]
+        )
+    );
+    Configuration::addRule(
+        new Rule(
+            new FixedFileHandler('/var/www/html/static/index.html'),
+            [
+                new RequestMatcher('GET', '/', 'exact'),
+            ]
+        )
+    );
+}
