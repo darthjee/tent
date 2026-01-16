3ed07a52c2f37f3446457753d15e3fe768651a4b
darthjee <darthjee@gmail.com>
2026-01-16 16:09:46 -0300

    Fix test

--- source/tests/unit/lib/models/Rule/RuleBuildTest.php
+++ source/tests/unit/lib/models/Rule/RuleBuildTest.php
@@ -9,28 +9,31 @@ use Tent\Handlers\ProxyRequestHandler;
 
 class RuleBuildTest extends TestCase
 {
-    public function testBuildCreatesRuleWithNamedParameters()
+    public function testBuildCreatesRuleWithProxyHandler()
     {
         $rule = Rule::build([
-            'host' => 'http://api.com',
-            'rules' => [
+            'handler' => [
+                'type' => 'proxy',
+                'host' => 'http://api.com'
+            ],
+            'matchers' => [
                 ['method' => 'GET', 'uri' => '/index.html', 'type' => 'exact'],
                 ['method' => 'POST', 'uri' => '/submit', 'type' => 'begins_with']
             ]
         ]);
-
+    
         $this->assertInstanceOf(Rule::class, $rule);
         $handler = $rule->handler();
         $this->assertInstanceOf(ProxyRequestHandler::class, $handler);
-
+    
         $requestGet = $this->createMock(Request::class);
         $requestGet->method('requestMethod')->willReturn('GET');
         $requestGet->method('requestUrl')->willReturn('/index.html');
-
+    
         $requestPost = $this->createMock(Request::class);
         $requestPost->method('requestMethod')->willReturn('POST');
         $requestPost->method('requestUrl')->willReturn('/submit/123');
-
+    
         $this->assertTrue($rule->match($requestGet));
         $this->assertTrue($rule->match($requestPost));
     }
