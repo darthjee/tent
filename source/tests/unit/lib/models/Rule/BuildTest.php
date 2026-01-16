de62e63930e8bece0dcfebd3becd21c6c224d135
darthjee <darthjee@gmail.com>
2026-01-16 15:06:15 -0300

    Add  tests

---
+++ source/tests/unit/lib/models/Rule/BuildTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tent\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Tent\Models\Rule;
+
+class BuildTest extends TestCase
+{
+    public function testBuildCreatesRuleWithNamedParameters()
+    {
+        $rule = Rule::build([
+            'host' => 'http://api.com',
+            'rules' => [
+                ['method' => 'GET', 'uri' => '/index.html', 'type' => 'exact'],
+                ['method' => 'POST', 'uri' => '/submit', 'type' => 'begins_with']
+            ]
+        ]);
+
+        $this->assertInstanceOf(Rule::class, $rule);
+        $handler = $rule->handler();
+        $this->assertInstanceOf(\Tent\Handlers\ProxyRequestHandler::class, $handler);
+
+        $requestGet = $this->createMock(\Tent\Models\Request::class);
+        $requestGet->method('requestMethod')->willReturn('GET');
+        $requestGet->method('requestUrl')->willReturn('/index.html');
+
+        $requestPost = $this->createMock(\Tent\Models\Request::class);
+        $requestPost->method('requestMethod')->willReturn('POST');
+        $requestPost->method('requestUrl')->willReturn('/submit/123');
+
+        $this->assertTrue($rule->match($requestGet));
+        $this->assertTrue($rule->match($requestPost));
+    }
+}
