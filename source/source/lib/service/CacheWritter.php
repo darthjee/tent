5699f23a54257aa8e5fc3c01580b2d75dac7e946
darthjee <darthjee@gmail.com>
2026-01-23 20:34:50 -0300

    Initialize

--- source/source/lib/service/CacheWritter.php
+++ source/source/lib/service/CacheWritter.php
@@ -2,6 +2,18 @@
 
 namespace Tent\Service;
 
+use Tent\Models\Response;
+use Tent\Models\FileCache;
+use Response;
+
 class CacheWritter
 {
+    private Response $response;
+    private FileCache $cache;
+
+    public function __construct(Response $response, FileCache $cache)
+    {
+        $this->response = $response;
+        $this->cache = $cache;
+    }
 }
