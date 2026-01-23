3ac21f94470c0d543f65f4295121f85ffe40852b
darthjee <darthjee@gmail.com>
2026-01-23 19:33:08 -0300

    Add test

---
+++ source/tests/unit/lib/utils/FilePathTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tent\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Tent\Utils\FilePath;
+use Tent\Models\FolderLocation;
+
+class FilePathTest extends TestCase
+{
+    public function testGetFullPathConcatenatesBasePathAndFilePath()
+    {
+        $location = new FolderLocation('/var/www/');
+        $this->assertEquals('/var/www/index.html', FilePath::getFullPath('index.html', $location));
+    }
+
+    public function testGetFullPathWithTrailingSlashInBasePath()
+    {
+        $location = new FolderLocation('/var/www/');
+        $this->assertEquals('/var/www/assets/style.css', FilePath::getFullPath('assets/style.css', $location));
+    }
+
+    public function testGetFullPathWithNoTrailingSlashInBasePath()
+    {
+        $location = new FolderLocation('/var/www');
+        $this->assertEquals('/var/www/assets/app.js', FilePath::getFullPath('/assets/app.js', $location));
+    }
+}
