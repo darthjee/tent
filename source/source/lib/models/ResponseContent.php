377b216f5181665e302b3a1c823545d1f2f6d4ea
darthjee <darthjee@gmail.com>
2026-01-23 19:09:12 -0300

    Add documentation

--- source/source/lib/models/ResponseContent.php
+++ source/source/lib/models/ResponseContent.php
@@ -4,6 +4,17 @@ namespace Tent\Models;
 
 interface ResponseContent
 {
+    /**
+     * Returns the content of the response.
+     *
+     * @return string
+     */
     public function content(): string;
+
+    /**
+     * Returns the headers associated with the response content.
+     *
+     * @return array
+     */
     public function headers(): array;
 }
