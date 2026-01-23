377b33a520d9e0d20287007a8b5edb16983dfe54
darthjee <darthjee@gmail.com>
2026-01-23 19:48:05 -0300

    Fix spec

--- source/tests/unit/lib/models/FileCacheTest.php
+++ source/tests/unit/lib/models/FileCacheTest.php
@@ -9,10 +9,13 @@ use Tent\Models\FolderLocation;
 class FileCacheTest extends TestCase
 {
     private $basePath;
+    private $path;
 
     public function setUp(): void
     {
         $this->basePath = sys_get_temp_dir() . '/tent_cache_' . uniqid();
+        $this->path = 'some_file.txt';
+
         mkdir($this->basePath);
         file_put_contents($this->basePath . '/cache.body.txt', 'Cached body content');
     }
@@ -27,7 +30,7 @@ class FileCacheTest extends TestCase
     public function testContentReadsCacheBodyFile()
     {
         $location = new FolderLocation($this->basePath);
-        $cache = new FileCache('ignored', $location);
+        $cache = new FileCache($this->path, $location);
         $this->assertEquals('Cached body content', $cache->content());
     }
 }
