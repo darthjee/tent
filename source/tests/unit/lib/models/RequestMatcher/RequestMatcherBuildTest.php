dd473e94b855d6d8386f62c69c85561a014c2c05
darthjee <darthjee@gmail.com>
2026-01-23 10:08:48 -0300

    Lint fix

--- source/tests/unit/lib/models/RequestMatcher/RequestMatcherBuildTest.php
+++ source/tests/unit/lib/models/RequestMatcher/RequestMatcherBuildTest.php
@@ -37,39 +37,39 @@ class RequestMatcherBuildTest extends TestCase
         $this->assertTrue($matcher->matches($request));
     }
 
-        public function testBuildMatchersCreatesMultipleMatchers()
-        {
-            $attributes = [
-                ['method' => 'GET', 'uri' => '/users', 'type' => 'exact'],
-                ['method' => 'POST', 'uri' => '/users', 'type' => 'begins_with'],
-                ['method' => null, 'uri' => '/admin', 'type' => 'exact'],
-            ];
+    public function testBuildMatchersCreatesMultipleMatchers()
+    {
+        $attributes = [
+            ['method' => 'GET', 'uri' => '/users', 'type' => 'exact'],
+            ['method' => 'POST', 'uri' => '/users', 'type' => 'begins_with'],
+            ['method' => null, 'uri' => '/admin', 'type' => 'exact'],
+        ];
 
-            $matchers = RequestMatcher::buildMatchers($attributes);
+        $matchers = RequestMatcher::buildMatchers($attributes);
 
-            $this->assertCount(3, $matchers);
-            $this->assertInstanceOf(RequestMatcher::class, $matchers[0]);
-            $this->assertInstanceOf(RequestMatcher::class, $matchers[1]);
-            $this->assertInstanceOf(RequestMatcher::class, $matchers[2]);
+        $this->assertCount(3, $matchers);
+        $this->assertInstanceOf(RequestMatcher::class, $matchers[0]);
+        $this->assertInstanceOf(RequestMatcher::class, $matchers[1]);
+        $this->assertInstanceOf(RequestMatcher::class, $matchers[2]);
 
-            $this->assertEquals('GET', $this->getPrivateProperty($matchers[0], 'requestMethod'));
-            $this->assertEquals('/users', $this->getPrivateProperty($matchers[0], 'requestUri'));
-            $this->assertEquals('exact', $this->getPrivateProperty($matchers[0], 'matchType'));
+        $this->assertEquals('GET', $this->getPrivateProperty($matchers[0], 'requestMethod'));
+        $this->assertEquals('/users', $this->getPrivateProperty($matchers[0], 'requestUri'));
+        $this->assertEquals('exact', $this->getPrivateProperty($matchers[0], 'matchType'));
 
-            $this->assertEquals('POST', $this->getPrivateProperty($matchers[1], 'requestMethod'));
-            $this->assertEquals('/users', $this->getPrivateProperty($matchers[1], 'requestUri'));
-            $this->assertEquals('begins_with', $this->getPrivateProperty($matchers[1], 'matchType'));
+        $this->assertEquals('POST', $this->getPrivateProperty($matchers[1], 'requestMethod'));
+        $this->assertEquals('/users', $this->getPrivateProperty($matchers[1], 'requestUri'));
+        $this->assertEquals('begins_with', $this->getPrivateProperty($matchers[1], 'matchType'));
 
-            $this->assertNull($this->getPrivateProperty($matchers[2], 'requestMethod'));
-            $this->assertEquals('/admin', $this->getPrivateProperty($matchers[2], 'requestUri'));
-            $this->assertEquals('exact', $this->getPrivateProperty($matchers[2], 'matchType'));
-        }
+        $this->assertNull($this->getPrivateProperty($matchers[2], 'requestMethod'));
+        $this->assertEquals('/admin', $this->getPrivateProperty($matchers[2], 'requestUri'));
+        $this->assertEquals('exact', $this->getPrivateProperty($matchers[2], 'matchType'));
+    }
 
-        private function getPrivateProperty($object, $property)
-        {
-            $reflection = new \ReflectionClass($object);
-            $prop = $reflection->getProperty($property);
-            $prop->setAccessible(true);
-            return $prop->getValue($object);
-        }
+    private function getPrivateProperty($object, $property)
+    {
+        $reflection = new \ReflectionClass($object);
+        $prop = $reflection->getProperty($property);
+        $prop->setAccessible(true);
+        return $prop->getValue($object);
+    }
 }
