7d7a70078385482c39322e643f8d8873151b08e4
darthjee <darthjee@gmail.com>
2026-01-23 14:07:10 -0300

    Add documentation

--- source/source/lib/models/File.php
+++ source/source/lib/models/File.php
@@ -1,19 +1,44 @@
 <?php
 
+
 namespace Tent\Models;
 use Tent\Models\FolderLocation;
 
+
+/**
+ * Represents a file within a folder location.
+ *
+ * Used to combine a file path with a base folder location, providing the full path to the file.
+ */
 class File
 {
+    /**
+     * @var string Relative or absolute file path.
+     */
     private string $path;
+
+    /**
+     * @var FolderLocation The base folder location.
+     */
     private FolderLocation $location;
 
+    /**
+     * Constructs a File object.
+     *
+     * @param string $path Relative or absolute file path.
+     * @param FolderLocation $location The base folder location.
+     */
     public function __construct(string $path, FolderLocation $location)
     {
         $this->path = $path;
         $this->location = $location;
     }
 
+    /**
+     * Returns the full path to the file, combining the base folder and file path.
+     *
+     * @return string The full file path.
+     */
     public function fullPath(): string
     {
         return $this->location->basePath() . $this->path;
