3b7cbaf732b07f94b943736d94a314d00808016e
darthjee <darthjee@gmail.com>
2026-01-23 22:03:39 -0300

    Add test

---
+++ source/tests/unit/lib/middlewares/FileCacheMiddleware/FileCacheMiddlewareProcessResponseTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tent\Tests\Middlewares;
+
+use PHPUnit\Framework\TestCase;
+use Tent\Middlewares\FileCacheMiddleware;
+use Tent\Models\FolderLocation;
+use Tent\Models\Response;
+use Tent\Models\ProcessingRequest;
+use Tent\Models\FileCache;
+
+class FileCacheMiddlewareProcessResponseTest extends TestCase
+{
+    private $cacheDir;
+    private $location;
+
+    protected function setUp(): void
+    {
+        $this->cacheDir = sys_get_temp_dir() . '/filecache_middleware_test_' . uniqid();
+        mkdir($this->cacheDir);
+        $this->location = new FolderLocation($this->cacheDir);
+    }
+
+    protected function tearDown(): void
+    {
+        array_map('unlink', glob($this->cacheDir . '/*'));
+        rmdir($this->cacheDir);
+    }
+
+    public function testProcessResponseStoresCache()
+    {
+        $path = '/file.txt';
+        $headers = ['Content-Type: text/plain', 'Content-Length: 11'];
+        $response = new Response('cached body', 200, $headers);
+        $request = $this->createMock(ProcessingRequest::class);
+        $request->method('path')->willReturn($path);
+
+        $middleware = new FileCacheMiddleware($this->location);
+        $middleware->processResponse($response);
+
+        $cache = new FileCache($path, $this->location);
+        $this->assertTrue($cache->exists());
+        $this->assertEquals('cached body', $cache->content());
+        $this->assertEquals($headers, $cache->headers());
+    }
+}
