59b58bcecc4559f8d5a1c5319df42737d33fa950
darthjee <darthjee@gmail.com>
2026-01-24 20:20:18 -0300

    Add documentation

--- source/source/lib/middlewares/FileCacheMiddleware.php
+++ source/source/lib/middlewares/FileCacheMiddleware.php
@@ -7,25 +7,54 @@ use Tent\Models\FolderLocation;
 use Tent\Models\FileCache;
 use Tent\Models\Response;
 
+/**
+ * Middleware for caching responses to files.
+ */
 class FileCacheMiddleware extends Middleware
 {
     private FolderLocation $location;
 
+    /**
+     * Constructs a FileCacheMiddleware instance.
+     *
+     * @param FolderLocation $location The base folder location for caching.
+     */
     public function __construct(FolderLocation $location)
     {
         $this->location = $location;
     }
 
+    /**
+     * Builds a FileCacheMiddleware instance from the given attributes.
+     *
+     * @param array $attributes The attributes to build the middleware.
+     * @return FileCacheMiddleware The constructed FileCacheMiddleware instance.
+     */
     public static function build(array $attributes): FileCacheMiddleware
     {
         return new self($attributes['location'] ?? null);
     }
 
+    /**
+     * Processes the incoming request.
+     *
+     * In the future, this method will check if a cached response exists for the incoming request
+     * and return it if available. Currently, it returns the request unmodified.
+     *
+     * @param ProcessingRequest $request The incoming processing request.
+     * @return ProcessingRequest The (potentially cached) processing request.
+     */
     public function processRequest(ProcessingRequest $request): ProcessingRequest
     {
         return $request;
     }
 
+    /**
+     * Caches the response to a file.
+     *
+     * @param Response $response The response to cache.
+     * @return Response The original response.
+     */
     public function processResponse(Response $response): Response
     {
         if ($response) {
