0adf238f5fe708138d43e5df607718dc0c9afb0d
darthjee <darthjee@gmail.com>
2026-01-25 18:08:14 -0300

    Add test

---
+++ source/tests/unit/lib/utils/HttpCodeMatcherTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tent\Tests\Utils;
+
+use PHPUnit\Framework\TestCase;
+use Tent\Utils\HttpCodeMatcher;
+
+class HttpCodeMatcherTest extends TestCase
+{
+    public function testMatchReturnsTrueWhenCodeIsInList()
+    {
+        $this->assertTrue(HttpCodeMatcher::match(200, [200]));
+        $this->assertTrue(HttpCodeMatcher::match(201, [201]));
+        $this->assertTrue(HttpCodeMatcher::match(200, [200, 201]));
+    }
+
+    public function testMatchReturnsFalseWhenCodeIsNotInList()
+    {
+        $this->assertFalse(HttpCodeMatcher::match(200, [201]));
+        $this->assertFalse(HttpCodeMatcher::match(201, [200]));
+        $this->assertFalse(HttpCodeMatcher::match(200, []));
+        $this->assertFalse(HttpCodeMatcher::match(201, []));
+    }
+}
