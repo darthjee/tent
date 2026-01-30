3dbde3245f19ba574044b85e4b400a9ee3fa8f4a
darthjee <darthjee@gmail.com>
2026-01-30 20:19:34 -0300

    Add ResponseCacher

---
+++ source/source/lib/service/ResponseCacher.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tent\Service;
+
+use Tent\Models\Cache;
+use Tent\Models\Response;
+
+class ResponseCacher
+{
+    private Cache $cache;
+    private Response $response;
+
+    public function __construct(Cache $cache, Response $response)
+    {
+        $this->cache = $cache;
+        $this->$response = $response;
+    }
+}
