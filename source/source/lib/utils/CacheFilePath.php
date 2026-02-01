45b340b246d0e8fd67f5b2483d2d5065e9b7d087
darthjee <darthjee@gmail.com>
2026-02-01 18:17:04 -0300

    Remove old option

--- source/source/lib/utils/CacheFilePath.php
+++ source/source/lib/utils/CacheFilePath.php
@@ -9,7 +9,7 @@ class CacheFilePath
     /**
      * Constructs the cache file path based on the type, base path, and query.
      *
-     * @param string $type     The type of cache file ('body' or 'headers').
+     * @param string $type     The type of cache file ('body' or 'meta').
      * @param string $basePath The base path where the cache files are stored.
      * @param string $query    The query string associated with the request.
      *
@@ -23,8 +23,6 @@ class CacheFilePath
         switch ($type) {
             case 'body':
                 return $basePath . '/' . $queryHash . '.body.txt';
-            case 'headers':
-                return $basePath . '/' . $queryHash . '.headers.json';
             case 'meta':
                 return $basePath . '/' . $queryHash . '.meta.json';
             default:
