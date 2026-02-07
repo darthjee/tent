daba5e1a12337250de5afae4457402e4904dda77
darthjee <darthjee@gmail.com>
2026-02-07 15:51:36 -0300

    Add middleware eamples

--- source/source/lib/middlewares/Middleware.php
+++ source/source/lib/middlewares/Middleware.php
@@ -6,7 +6,83 @@ use Tent\Models\ProcessingRequest;
 use Tent\Models\Response;
 
 /**
- * Interface for request middlewares that can process or modify a ProcessingRequest.
+ * Base class for Tent middlewares that can process or modify requests and responses.
+ *
+ * ## Example: Request Middleware (prepend /api/ to path)
+ *
+ * ```php
+ * namespace Tent\Middlewares;
+ *
+ * use Tent\Models\ProcessingRequest;
+ *
+ * class PrependApiPathMiddleware extends Middleware
+ * {
+ *     private string $prepend;
+ *
+ *     public function __construct(string $prepend = '/api')
+ *     {
+ *         $this->prepend = $prepend;
+ *     }
+ *
+ *     public static function build(array $attributes): self
+ *     {
+ *         return new self($attributes['prepend'] ?? '/api');
+ *     }
+ *
+ *     public function processRequest(ProcessingRequest $request): ProcessingRequest
+ *     {
+ *         $path = $request->requestPath();
+ *         if (strpos($path, $this->prepend . '/') !== 0) {
+ *             $request->setRequestPath($this->prepend . $path);
+ *         }
+ *         return $request;
+ *     }
+ *
+ *     // Optionally implement processResponse if needed
+ * }
+ * ```
+ *
+ * ## Example: Response Middleware (simple header)
+ *
+ * ```php
+ * namespace Tent\Middlewares;
+ *
+ * use Tent\Models\Response;
+ *
+ * class AddPoweredByHeaderMiddleware extends Middleware
+ * {
+ *     public function processResponse(Response $response): Response
+ *     {
+ *         $response->setHeader('X-Powered-By', 'Tent');
+ *         return $response;
+ *     }
+ * }
+ * ```
+ *
+ * ## Usage in configuration
+ *
+ * ```php
+ * Configuration::buildRule([
+ *     'handler' => [
+ *         'type' => 'proxy',
+ *         'host' => 'http://api:80'
+ *     ],
+ *     'matchers' => [
+ *         ['method' => 'GET', 'uri' => '/persons', 'type' => 'exact']
+ *     ],
+ *     'middlewares' => [
+ *         [
+ *             'class' => 'Tent\\Middlewares\\PrependApiPathMiddleware',
+ *             'prepend' => '/api'
+ *         ],
+ *         [
+ *             'class' => 'Tent\\Middlewares\\AddPoweredByHeaderMiddleware'
+ *         ]
+ *     ]
+ * ]);
+ * ```
+ *
+ * Middlewares can override either processRequest, processResponse, or both.
  */
 abstract class Middleware
 {
