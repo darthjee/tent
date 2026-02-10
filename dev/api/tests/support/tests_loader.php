ccafff19aa831990df5ae568757f4de04cd4c47f
Fernando Favini <darthjee@gmail.com>
2026-02-10 20:52:39 -0300

    Reorganize DEV Api loaders (#113)

--- dev/api/tests/support/tests_loader.php
+++ dev/api/tests/support/tests_loader.php
@@ -2,22 +2,9 @@
 
 // tests_loader.php
 
-require_once __DIR__ . '/../../source/lib/api_dev/models/Response.php';
+require_once __DIR__ . '/../../source/loader.php';
 
-require_once __DIR__ . '/../../source/lib/api_dev/Configuration.php';
-require_once __DIR__ . '/../../source/lib/api_dev/Endpoint.php';
-require_once __DIR__ . '/../../source/lib/api_dev/endpoints/HealthCheckEndpoint.php';
-require_once __DIR__ . '/../../source/lib/api_dev/endpoints/ListPersonsEndpoint.php';
-require_once __DIR__ . '/../../source/lib/api_dev/models/MissingResponse.php';
-require_once __DIR__ . '/../../source/lib/api_dev/models/Person.php';
-require_once __DIR__ . '/../../source/lib/api_dev/models/Request.php';
-require_once __DIR__ . '/../../source/lib/api_dev/RequestHandler.php';
-require_once __DIR__ . '/../../source/lib/api_dev/Route.php';
-require_once __DIR__ . '/../../source/lib/api_dev/RouteConfiguration.php';
-require_once __DIR__ . '/../../source/lib/mysql/Configuration.php';
-require_once __DIR__ . '/../../source/lib/mysql/Connection.php';
 require_once __DIR__ . '/../../source/lib/mysql/Migration.php';
 require_once __DIR__ . '/../../source/lib/mysql/MigrationsProcessor.php';
-require_once __DIR__ . '/../../source/lib/mysql/ModelConnection.php';
 
 require_once __DIR__ . '/database_initializer.php';
