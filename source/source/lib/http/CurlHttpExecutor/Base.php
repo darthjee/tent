9cfce791fe33e46a881fb37de142aa773fa148d6
Fernando Favini <darthjee@gmail.com>
2026-02-11 18:05:43 -0300

    Init executor split

---
+++ source/source/lib/http/CurlHttpExecutor/Base.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tent\Http;
+
+use Tent\Utils\CurlUtils\CurlHttpExecutor;
+use CurlHandle;
+
+abstract class Base
+{
+    private string $url;
+    private array $headers;
+    private ?string $body;
+    private ?CurlHandle $curlHandle;
+
+    public function __construct(array $options)
+    {
+        $this->url = $options['url'] ?? '';
+        $this->headers = $options['headers'] ?? [];
+        $this->body = $options['body'] ?? null;
+    }
+
+    abstract public function request();
+
+    private function initCurlRequest()
+    {
+        $headerLines = CurlUtils::buildHeaderLines($this->headers);
+
+        $this->curlHandle = curl_init($this->url);
+
+        curl_setopt($this->curlHandle, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->curlHandle, CURLOPT_HEADER, true);
+        curl_setopt($this->curlHandle, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($this->curlHandle, CURLOPT_HTTPHEADER, $headerLines);
+    }
+
+    private function executeCurlRequest()
+    {
+        $response = curl_exec($this->curlHandle);
+        $headerSize = curl_getinfo($this->curlHandle, CURLINFO_HEADER_SIZE);
+        $httpCode = curl_getinfo($this->curlHandle, CURLINFO_HTTP_CODE);
+
+        $headers = substr($response, 0, $headerSize);
+        $responseBody = substr($response, $headerSize);
+
+        curl_close($this->curlHandle);
+
+        $headerLines = CurlUtils::parseResponseHeaders($headers);
+
+        return [
+            'body' => $responseBody,
+            'httpCode' => $httpCode,
+            'headers' => $headerLines
+        ];
+    }
+}
