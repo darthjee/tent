9fc803c0e6a20f44c3f56ae36f65735d48f4f770
Fernando Favini <darthjee@gmail.com>
2026-02-11 07:51:33 -0300

    Add exception

--- dev/api/source/lib/api_dev/endpoints/CreatePersonEndpoint.php
+++ dev/api/source/lib/api_dev/endpoints/CreatePersonEndpoint.php
@@ -3,6 +3,7 @@
 namespace ApiDev;
 
 use ApiDev\Models\Person;
+use ApiDev\Exceptions\InvalidJsonException;
 
 class CreatePersonEndpoint extends Endpoint
 {
@@ -10,68 +11,71 @@ class CreatePersonEndpoint extends Endpoint
 
     public function handle()
     {
-        $this->data = json_decode($this->request->body(), true);
+        try {
+            $this->data = json_decode($this->request->body(), true);
+            if (!is_array($this->data)) {
+                throw new InvalidJsonException();
+            }
 
-        if (!is_array($this->data)) {
+            $attributes = [];
+
+            if (isset($this->data['first_name'])) {
+                $attributes['first_name'] = $this->data['first_name'];
+            }
+
+            if (isset($this->data['last_name'])) {
+                $attributes['last_name'] = $this->data['last_name'];
+            }
+
+            if (isset($this->data['birthdate'])) {
+                $attributes['birthdate'] = $this->data['birthdate'];
+            }
+
+            if (empty($attributes)) {
+                return new Response(
+                    json_encode(['error' => 'At least one field required']),
+                    400,
+                    ['Content-Type: application/json']
+                );
+            }
+
+            $id = Person::getConnection()->insert($attributes);
+
+            $persons = Person::getConnection()->getConnection()->fetchAll(
+                "SELECT * FROM persons WHERE id = ?",
+                [$id]
+            );
+
+            if (empty($persons)) {
+                return new Response(
+                    json_encode(['error' => 'Failed to retrieve created person']),
+                    500,
+                    ['Content-Type: application/json']
+                );
+            }
+
+            $person = new Person($persons[0]);
+
+            $responseData = [
+                'id' => $person->getId(),
+                'first_name' => $person->getFirstName(),
+                'last_name' => $person->getLastName(),
+                'birthdate' => $person->getBirthdate(),
+                'created_at' => $person->getCreatedAt(),
+                'updated_at' => $person->getUpdatedAt(),
+            ];
+
+            return new Response(
+                json_encode($responseData),
+                201,
+                ['Content-Type: application/json']
+            );
+        } catch (InvalidJsonException $e) {
             return new Response(
                 json_encode(['error' => 'Invalid JSON body']),
                 400,
                 ['Content-Type: application/json']
             );
         }
-
-        $attributes = [];
-
-        if (isset($this->data['first_name'])) {
-            $attributes['first_name'] = $this->data['first_name'];
-        }
-
-        if (isset($this->data['last_name'])) {
-            $attributes['last_name'] = $this->data['last_name'];
-        }
-
-        if (isset($this->data['birthdate'])) {
-            $attributes['birthdate'] = $this->data['birthdate'];
-        }
-
-        if (empty($attributes)) {
-            return new Response(
-                json_encode(['error' => 'At least one field required']),
-                400,
-                ['Content-Type: application/json']
-            );
-        }
-
-        $id = Person::getConnection()->insert($attributes);
-
-        $persons = Person::getConnection()->getConnection()->fetchAll(
-            "SELECT * FROM persons WHERE id = ?",
-            [$id]
-        );
-
-        if (empty($persons)) {
-            return new Response(
-                json_encode(['error' => 'Failed to retrieve created person']),
-                500,
-                ['Content-Type: application/json']
-            );
-        }
-
-        $person = new Person($persons[0]);
-
-        $responseData = [
-            'id' => $person->getId(),
-            'first_name' => $person->getFirstName(),
-            'last_name' => $person->getLastName(),
-            'birthdate' => $person->getBirthdate(),
-            'created_at' => $person->getCreatedAt(),
-            'updated_at' => $person->getUpdatedAt(),
-        ];
-
-        return new Response(
-            json_encode($responseData),
-            201,
-            ['Content-Type: application/json']
-        );
     }
 }
--- dev/api/source/loader.php
+++ dev/api/source/loader.php
@@ -7,6 +7,7 @@ require_once __DIR__ . '/lib/api_dev/Endpoint.php';
 require_once __DIR__ . '/lib/api_dev/endpoints/HealthCheckEndpoint.php';
 require_once __DIR__ . '/lib/api_dev/endpoints/ListPersonsEndpoint.php';
 require_once __DIR__ . '/lib/api_dev/endpoints/CreatePersonEndpoint.php';
+require_once __DIR__ . '/lib/api_dev/exceptions/InvalidJsonException.php';
 require_once __DIR__ . '/lib/api_dev/models/MissingResponse.php';
 require_once __DIR__ . '/lib/api_dev/models/Person.php';
 require_once __DIR__ . '/lib/api_dev/models/RequestInterface.php';
