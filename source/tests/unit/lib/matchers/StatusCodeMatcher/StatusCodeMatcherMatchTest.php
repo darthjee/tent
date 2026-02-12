a253660be33542002fcf9838c14aa77f0b68e965
darthjee <darthjee@gmail.com>
2026-02-11 21:25:01 -0300

    Split test

--- source/tests/unit/lib/matchers/StatusCodeMatcher/StatusCodeMatcherMatchTest.php
+++ source/tests/unit/lib/matchers/StatusCodeMatcher/StatusCodeMatcherMatchTest.php
@@ -2,13 +2,13 @@
 
 namespace Tent\Tests\Matchers;
 
-require_once __DIR__ . '/../../../support/loader.php';
+require_once __DIR__ . '/../../../../support/loader.php';
 
 use PHPUnit\Framework\TestCase;
 use Tent\Matchers\StatusCodeMatcher;
 use Tent\Models\Response;
 
-class StatusCodeMatcherTest extends TestCase
+class StatusCodeMatcherMatchTest extends TestCase
 {
     private function mockResponse($code)
     {
@@ -85,21 +85,5 @@ class StatusCodeMatcherTest extends TestCase
         $this->assertTrue($matcher->match($this->mockResponse(599)));
         $this->assertFalse($matcher->match($this->mockResponse(600)));
         $this->assertFalse($matcher->match($this->mockResponse(499)));
-    }
-
-    //////// Build tests
-    public function testBuildCreatesMatcherWithGivenCodes()
-    {
-        $matcher = StatusCodeMatcher::build(['httpCodes' => [201, 202]]);
-        $this->assertTrue($matcher->match($this->mockResponse(201)));
-        $this->assertTrue($matcher->match($this->mockResponse(202)));
-        $this->assertFalse($matcher->match($this->mockResponse(200)));
-    }
-
-    public function testBuildDefaultsTo200()
-    {
-        $matcher = StatusCodeMatcher::build([]);
-        $this->assertTrue($matcher->match($this->mockResponse(200)));
-        $this->assertFalse($matcher->match($this->mockResponse(201)));
     }
 }
