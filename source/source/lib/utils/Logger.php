36304391619dbfafa667734bd426bb18fea4cdca
darthjee <darthjee@gmail.com>
2026-02-11 22:23:25 -0300

    Add docs

--- source/source/lib/utils/Logger.php
+++ source/source/lib/utils/Logger.php
@@ -6,11 +6,21 @@ class Logger implements LoggerInterface
 {
     static private $instance;
 
+    /**
+     * Logs a deprecation message.
+     *
+     * @param string $message The deprecation message to log.
+     */
     public static function deprecate(string $message): void
     {
         self::getInstance()->logDeprecation($message);
     }
 
+    /**
+     * Gets the singleton instance of the Logger.
+     *
+     * @return LoggerInterface The singleton Logger instance.
+     */
     public static function getInstance(): LoggerInterface
     {
         if (!self::$instance) {
@@ -19,11 +29,21 @@ class Logger implements LoggerInterface
         return self::$instance;
     }
 
+    /**
+     * Sets the Logger instance to use for logging.
+     *
+     * @param LoggerInterface $logger The Logger instance to use.
+     */
     public static function setInstance(LoggerInterface $logger): void
     {
         self::$instance = $logger;
     }
 
+    /**
+     * Logs a deprecation message using trigger_error.
+     *
+     * @param string $message The deprecation message to log.
+     */
     public function logDeprecation(string $message): void
     {
         trigger_error($message, E_USER_DEPRECATED);
