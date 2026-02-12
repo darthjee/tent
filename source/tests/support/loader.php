3a978f5974f29b0f2d609dfcf313df971d9b9ced
Fernando Favini <darthjee@gmail.com>
2026-02-12 12:26:27 -0300

    Fix imports

--- source/tests/support/loader.php
+++ source/tests/support/loader.php
@@ -4,5 +4,7 @@ require_once __DIR__ . '/../../source/loader.php';
 
 require_once __DIR__ . '/utils/FileSystemUtils.php';
 require_once __DIR__ . '/utils/VoidLogger.php';
+require_once __DIR__ . '/handlers/RequestToBodyHandler.php';
+require_once __DIR__ . '/middlewares/DummyRequestMiddleware.php';
 
 require_once __DIR__ . '/initialize.php';
