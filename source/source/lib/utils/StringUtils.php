8a5ff7cfbbf68de727e5020d899edac43067b358
Fernando Favini <darthjee@gmail.com>
2026-02-23 18:25:58 -0300

    Add docs

--- source/source/lib/utils/StringUtils.php
+++ source/source/lib/utils/StringUtils.php
@@ -4,6 +4,16 @@ namespace Tent\Utils;
 
 class StringUtils
 {
+    /**
+    * Converts a string to StudlyCase (PascalCase).
+    *
+    * Example:
+    *   'begins_with' => 'BeginsWith'
+    *   'ENDS-with' => 'EndsWith'
+    *
+    * @param string $value The input string to convert.
+    * @return string The converted StudlyCase string.
+    */
     public static function toStudlyCase(string $value): string
     {
         $parts = preg_split('/[^a-z0-9]+/i', $value, -1, PREG_SPLIT_NO_EMPTY);
