a36e1b5aeaaf0fc21bf04a3d5dbbf0eeb33e413b
Fernando Favini <darthjee@gmail.com>
2026-03-04 20:52:08 -0300

    Rewrite spec

--- source/tests/unit/lib/request_handlers/DefaultProxyRequestHandler/DefaultProxyRequestHandlerTest.php
+++ source/tests/unit/lib/request_handlers/DefaultProxyRequestHandler/DefaultProxyRequestHandlerTest.php
@@ -6,117 +6,134 @@ require_once __DIR__ . '/../../../../support/loader.php';
 
 use PHPUnit\Framework\TestCase;
 use Tent\RequestHandlers\DefaultProxyRequestHandler;
-use Tent\RequestHandlers\ProxyRequestHandler;
-use Tent\Middlewares\RenameHeaderMiddleware;
-use Tent\Middlewares\SetHeadersMiddleware;
-use Tent\Middlewares\FileCacheMiddleware;
 use Tent\Models\ProcessingRequest;
 use Tent\Models\Response;
 use Tent\Http\HttpClientInterface;
 
 class DefaultProxyRequestHandlerTest extends TestCase
 {
-    public function testExtendsProxyRequestHandler()
+    private ?string $host = null;
+    private ?ProcessingRequest $request = null;
+    private ?HttpClientInterface $httpClient = null;
+    private ?string $requestMethod = null;
+    private ?string $requestPath = null;
+    private ?string $requestQuery = null;
+    private ?array $requestHeaders = null;
+    private ?string $requestBody = null;
+
+    public function testHandleRequestBuildsCorrectUrlWithoutCache()
     {
-        $handler = new DefaultProxyRequestHandler('http://api:80', false);
-        $this->assertInstanceOf(ProxyRequestHandler::class, $handler);
-    }
+        $this->initVariables();
+        $this->createMockHttpClient(
+            ['body' => 'response body', 'httpCode' => 200, 'headers' => []]
+        );
 
-    public function testMiddlewareOrderWithCacheEnabled()
-    {
-        $handler = new DefaultProxyRequestHandler('http://api:80', './cache', ['2xx']);
+        $handler = new DefaultProxyRequestHandler($this->host, false, ['2xx'], $this->httpClient);
+        $response = $handler->handleRequest($this->request);
 
-        $reflection = new \ReflectionClass($handler);
-        $prop = $reflection->getProperty('middlewares');
-        $prop->setAccessible(true);
-        $middlewares = $prop->getValue($handler);
-
-        $this->assertCount(3, $middlewares);
-        $this->assertInstanceOf(RenameHeaderMiddleware::class, $middlewares[0]);
-        $this->assertInstanceOf(SetHeadersMiddleware::class, $middlewares[1]);
-        $this->assertInstanceOf(FileCacheMiddleware::class, $middlewares[2]);
-    }
-
-    public function testMiddlewareOrderWithCacheDisabled()
-    {
-        $handler = new DefaultProxyRequestHandler('http://api:80', false);
-
-        $reflection = new \ReflectionClass($handler);
-        $prop = $reflection->getProperty('middlewares');
-        $prop->setAccessible(true);
-        $middlewares = $prop->getValue($handler);
-
-        $this->assertCount(2, $middlewares);
-        $this->assertInstanceOf(RenameHeaderMiddleware::class, $middlewares[0]);
-        $this->assertInstanceOf(SetHeadersMiddleware::class, $middlewares[1]);
-    }
-
-    public function testDefaultCacheDirectory()
-    {
-        $handler = new DefaultProxyRequestHandler('http://api:80');
-
-        $reflection = new \ReflectionClass($handler);
-        $prop = $reflection->getProperty('middlewares');
-        $prop->setAccessible(true);
-        $middlewares = $prop->getValue($handler);
-
-        // With defaults, caching should be enabled (3 middlewares)
-        $this->assertCount(3, $middlewares);
-        $this->assertInstanceOf(FileCacheMiddleware::class, $middlewares[2]);
-    }
-
-    public function testHostIsRenamedToXForwardedHost()
-    {
-        $httpClient = $this->createMock(HttpClientInterface::class);
-        $httpClient->expects($this->once())
-            ->method('request')
-            ->with(
-                'GET',
-                'http://api:80/test',
-                ['X-Forwarded-Host' => 'original.host', 'Host' => 'http://api:80'],
-                null
-            )
-            ->willReturn(['body' => '', 'httpCode' => 200, 'headers' => []]);
-
-        // We need to inject a mock http client; use reflection
-        $handler = new DefaultProxyRequestHandler('http://api:80', false, [], $httpClient);
-
-        $request = new ProcessingRequest([
-            'requestMethod' => 'GET',
-            'requestPath'   => '/test',
-            'query'         => '',
-            'headers'       => ['Host' => 'original.host'],
-        ]);
-
-        $response = $handler->handleRequest($request);
         $this->assertInstanceOf(Response::class, $response);
     }
 
-    public function testHostIsSetToConstructorValue()
+    public function testHandleRequestAppendsQueryStringWithoutCache()
     {
-        $httpClient = $this->createMock(HttpClientInterface::class);
-        $httpClient->expects($this->once())
-            ->method('request')
-            ->with(
-                'GET',
-                'http://api:80/test',
-                $this->callback(function ($headers) {
-                    return isset($headers['Host'])
-                        && $headers['Host'] === 'http://api:80';
-                })
-            )
-            ->willReturn(['body' => '', 'httpCode' => 200, 'headers' => []]);
+        $this->initVariables(['requestQuery' => 'page=1&limit=10']);
+        $this->createMockHttpClient(
+            ['body' => 'response body', 'httpCode' => 200, 'headers' => []]
+        );
 
-        $handler = new DefaultProxyRequestHandler('http://api:80', false, [], $httpClient);
+        $handler = new DefaultProxyRequestHandler($this->host, false, ['2xx'], $this->httpClient);
+        $response = $handler->handleRequest($this->request);
 
-        $request = new ProcessingRequest([
-            'requestMethod' => 'GET',
-            'requestPath'   => '/test',
-            'query'         => '',
-            'headers'       => ['Host' => 'original.host'],
+        $this->assertInstanceOf(Response::class, $response);
+    }
+
+    public function testHandleRequestAppliesDefaultHeaderMiddlewares()
+    {
+        $this->initVariables([
+            'requestHeaders' => [
+                'Host' => 'frontend.local:8080',
+                'Authorization' => 'Bearer token123'
+            ]
         ]);
 
-        $response = $handler->handleRequest($request);
+        $this->createMockHttpClient(
+            ['body' => 'created', 'httpCode' => 201, 'headers' => ['Location: /api/users/1']]
+        );
+
+        $handler = new DefaultProxyRequestHandler($this->host, false, ['2xx'], $this->httpClient);
+        $response = $handler->handleRequest($this->request);
+
         $this->assertInstanceOf(Response::class, $response);
+    }
+
+    public function testHandleRequestReturnsResponseWithCorrectData()
+    {
+        $this->initVariables();
+        $this->createMockHttpClient(
+            ['body' => '{"users": []}', 'httpCode' => 200, 'headers' => ['Content-Type: application/json']]
+        );
+
+        $handler = new DefaultProxyRequestHandler($this->host, false, ['2xx'], $this->httpClient);
+        $response = $handler->handleRequest($this->request);
+
+        $this->assertEquals('{"users": []}', $response->body());
+        $this->assertEquals(200, $response->httpCode());
+        $this->assertEquals(['Content-Type: application/json'], $response->headers());
+    }
+
+    private function createMockHttpClient(array $returnValue): void
+    {
+        $this->httpClient = $this->createMock(HttpClientInterface::class);
+        $expectedUrl = $this->host . $this->requestPath . ($this->requestQuery ? '?' . $this->requestQuery : '');
+        $expectedHeaders = $this->expectedHeadersAfterDefaultMiddlewares();
+
+        $this->httpClient->expects($this->once())
+            ->method('request')
+            ->with(
+                $this->requestMethod,
+                $expectedUrl,
+                $this->callback(function (array $headers) use ($expectedHeaders) {
+                    return $headers == $expectedHeaders;
+                }),
+                $this->requestBody
+            )
+            ->willReturn($returnValue);
+    }
+
+    private function expectedHeadersAfterDefaultMiddlewares(): array
+    {
+        $headers = $this->requestHeaders ?? [];
+
+        if (array_key_exists('Host', $headers)) {
+            $headers['X-Forwarded-Host'] = $headers['Host'];
+            unset($headers['Host']);
+        }
+
+        $headers['Host'] = $this->host;
+
+        return $headers;
+    }
+
+    private function initVariables(array $overrides = []): void
+    {
+        $this->requestMethod = $overrides['requestMethod'] ?? 'GET';
+        $this->requestPath = $overrides['requestPath'] ?? '/api/users';
+        $this->requestQuery = $overrides['requestQuery'] ?? '';
+        $this->requestHeaders = $overrides['requestHeaders'] ?? [];
+        $this->requestBody = $overrides['requestBody'] ?? null;
+        $this->host = $overrides['host'] ?? 'http://backend:8080';
+
+        $this->request = $this->buildProcessingRequest();
+    }
+
+    private function buildProcessingRequest(): ProcessingRequest
+    {
+        return new ProcessingRequest([
+            'requestMethod' => $this->requestMethod,
+            'body' => $this->requestBody,
+            'headers' => $this->requestHeaders,
+            'requestPath' => $this->requestPath,
+            'query' => $this->requestQuery
+        ]);
     }
 }
