9284c78e7c68ce6c5877c3f8378e32c009f1597f
Fernando Favini <darthjee@gmail.com>
2026-03-05 07:44:24 -0300

    Use new handler

--- docker_volumes/configuration/rules/backend.php
+++ docker_volumes/configuration/rules/backend.php
@@ -5,7 +5,7 @@ use Tent\Configuration;
 Configuration::buildRule([
     'handler' => [
         'type' => 'default_proxy',
-        'host' => 'http://api:80'
+        'host' => 'api:80'
     ],
     'matchers' => [
         ['method' => 'GET', 'uri' => '/persons', 'type' => 'exact']
@@ -15,8 +15,7 @@ Configuration::buildRule([
 Configuration::buildRule([
     'handler' => [
         'type' => 'default_proxy',
-        'host' => 'http://api:80',
-        'cache' => false
+        'host' => 'api:80'
     ],
     'matchers' => [
         ['method' => 'POST', 'uri' => '/persons', 'type' => 'exact']
