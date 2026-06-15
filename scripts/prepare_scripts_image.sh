cf37986191f3073e87a6fbc40601fdb5147fda01
darthjee <darthjee@gmail.com>
2026-06-15 16:08:57 -0300

    Add debug

--- scripts/prepare_scripts_image.sh
+++ scripts/prepare_scripts_image.sh
@@ -5,5 +5,7 @@ ARCH="${1:-arm64}"
 DOCKERFILE="dockerfiles/tent/Dockerfile"
 
 VERSION=$(sed -n 's/FROM darthjee\/scripts:\([^ ]*\) as.*/\1/p' "$DOCKERFILE")
+set -x
 docker pull "darthjee/scripts:${VERSION}-${ARCH}"
 docker tag "darthjee/scripts:${VERSION}-${ARCH}" "darthjee/scripts:${VERSION}"
+set +x
