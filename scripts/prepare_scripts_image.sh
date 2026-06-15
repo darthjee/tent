cd11d577fa7ff52b71c2de03fede4069c8d53da9
darthjee <darthjee@gmail.com>
2026-06-15 16:24:23 -0300

    Revert debug

--- scripts/prepare_scripts_image.sh
+++ scripts/prepare_scripts_image.sh
@@ -5,7 +5,5 @@ ARCH="${1:-arm64}"
 DOCKERFILE="dockerfiles/tent/Dockerfile"
 
 VERSION=$(sed -n 's/FROM darthjee\/scripts:\([^ ]*\) as.*/\1/p' "$DOCKERFILE")
-set -x
 docker pull "darthjee/scripts:${VERSION}-${ARCH}" --platform "linux/${ARCH}"
 docker tag "darthjee/scripts:${VERSION}-${ARCH}" "darthjee/scripts:${VERSION}"
-set +x
