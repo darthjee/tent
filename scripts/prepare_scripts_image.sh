e7857294c5f91734dda0c8c825d67c303f03db13
darthjee <darthjee@gmail.com>
2026-06-15 15:19:01 -0300

    Prepare to use arm64 scripts

---
+++ scripts/prepare_scripts_image.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ARCH="${1:-amd64}"
+DOCKERFILE="dockerfiles/tent/Dockerfile"
+
+VERSION=$(sed -n 's/FROM darthjee\/scripts:\([^ ]*\) as.*/\1/p' "$DOCKERFILE")
+docker pull "darthjee/scripts:${VERSION}-${ARCH}"
+docker tag "darthjee/scripts:${VERSION}-${ARCH}" "darthjee/scripts:${VERSION}"
